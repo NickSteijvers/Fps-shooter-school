cbebeca9ad1a69770afc0afdd578c2653e801e66
NickSteijvers <144125086+NickSteijvers@users.noreply.github.com>
2023-12-12 15:30:59 +0100

    victory

--- Fps_shooter_school/Assets/Scripts/Shooting.cs
+++ Fps_shooter_school/Assets/Scripts/Shooting.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Shooting : MonoBehaviour
 {
@@ -9,7 +10,8 @@ public class Shooting : MonoBehaviour
     private Ray ray;
     private RaycastHit hit;
 
-   
+    public int beans = 150;
+
     // Update is called once per frame
     void Update()
     {
@@ -22,9 +24,16 @@ public class Shooting : MonoBehaviour
                 {
                     Debug.Log("hit");
                     Destroy(hit.collider.gameObject);
+                    beans = beans - 1;
+                    Debug.Log(beans);
                 }
             }
         }
         
+      if (beans == 0) 
+        {
+            SceneManager.LoadScene(sceneName: "victory");
+            Debug.Log("Victory");
+        }
     }
 }
